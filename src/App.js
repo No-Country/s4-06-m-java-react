81b3f31e1a0f54a52d5f8c5b4097c8b95b313bdb
cristian <cristiangomez.dev@gmail.com>
2022-09-24 21:28:40 -0300

    setting redux and rtk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { RouterApp } from "./router/RouterApp";
+import { store } from './store/'
+import { Provider } from 'react-redux'
 
 export const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <RouterApp />
-    </>
+    </Provider>
   );
 };
