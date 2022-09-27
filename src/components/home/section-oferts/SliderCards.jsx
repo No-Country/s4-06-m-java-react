dbc07358d3a4db199e34820c694daf58621eaa99
cristian mosquera <cmosqbone1@gmail.com>
2022-09-28 00:00:21 +0200

    adding flashdeal

--- src/components/home/section-oferts/SliderCards.jsx
+++ src/components/home/section-oferts/SliderCards.jsx
@@ -1,9 +1,9 @@
-import "./SliderCards.css";
 import { Card } from "../../sharedComponents/card/Card";
 import TshirtYellow from "../../../assets/images/card/t-shirt1.png";
 import TshirtWhite from "../../../assets/images/card/t-shirt2.png";
 import TshirtRed from "../../../assets/images/card/t-shirt3.png";
 import TshirtWhiteAndGrenn from "../../../assets/images/card/t-shirt4.png";
+import "./SliderCards.css";
 export const SliderCards = () => {
   return (
     <div className="SliderCards">
