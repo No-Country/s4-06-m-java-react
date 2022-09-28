959ef168b30070bcb65665e0a2f86ec62a3e3996
cristian mosquera <cmosqbone1@gmail.com>
2022-09-28 22:08:01 +0200

    adding footer

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,14 +1,26 @@
 import { Route, Routes } from "react-router-dom";
+import { Footer } from "../components/sharedComponents/footer/Footer";
+import { Nav } from "../components/sharedComponents/nav/Nav";
 import { AuthScreen } from "../pages/AuthScreen";
 import { HomeScreen } from "../pages/HomeScreen";
 
 export const RouterApp = () => {
   return (
-    <>
+    <div className="container-principal">
+      {/*  nav compartido entre todas las paginas */}
+
+      <Nav />
+
+      {/* routes pagehome , prooductPage , detailsProduct */}
+
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/auth" element={<AuthScreen />} />
       </Routes>
-    </>
+
+      {/* footer compartido entre todas las paginas */}
+
+      <Footer />
+    </div>
   );
 };
