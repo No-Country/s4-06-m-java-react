405b875f9c41d2c997dcfc6462966af2fac86020
cristian mosquera <cmosqbone1@gmail.com>
2022-10-04 00:03:15 +0200

    counter down

--- src/components/home/section-flash-deal/FlashDeeal.jsx
+++ src/components/home/section-flash-deal/FlashDeeal.jsx
@@ -1,9 +1,47 @@
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { CardSimple } from "../../sharedComponents/card/CardSimple";
 
 import "./FlashDeeal.css";
 
 export const FlashDeeal = () => {
+  const [timeHours, setHours] = useState("");
+  const [timeMinutes, setMinutes] = useState("00");
+  const [timeSeconds, setSeconds] = useState("00");
+
+  let interval = useRef();
+
+  const startTime = () => {
+    const countdownDate = new Date("Dec 30 , 2022 04:00:00").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      console.log(minutes);
+      console.log(seconds);
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setHours(hours);
+        setMinutes(minutes);
+        setSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTime();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
   return (
     <div className="FlashDeeal">
       <div className="FlashDeeal__wrapper-deal">
@@ -14,16 +52,16 @@ export const FlashDeeal = () => {
 
         <div className="timer">
           <div className="timer-box">
-            <span className="timer-box__number">08</span>
-            <span className="timer-box__days">Days</span>
+            <span className="timer-box__number">{timeHours}</span>
+            <span className="timer-box__days">Horas</span>
           </div>
           <div className="timer-box">
-            <span className="timer-box__number">08</span>
-            <span className="timer-box__days">Days</span>
+            <span className="timer-box__number">{timeMinutes}</span>
+            <span className="timer-box__days">Minutos</span>
           </div>
           <div className="timer-box">
-            <span className="timer-box__number">08</span>
-            <span className="timer-box__days">Days</span>
+            <span className="timer-box__number">{timeSeconds}</span>
+            <span className="timer-box__days">Segundos</span>
           </div>
         </div>
         <Link to="/products">
