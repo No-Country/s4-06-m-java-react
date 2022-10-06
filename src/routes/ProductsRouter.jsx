23dd28f056850c4420284be510073d0576c30d92
unknown <santiagoschroh@gmail.com>
2022-10-06 10:37:03 -0300

    changes

--- src/routes/ProductsRouter.jsx
+++ src/routes/ProductsRouter.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import InputSearch from "../components/sharedComponents/inputSearch/InputSearch";
 
 import ProductsScreen from "../pages/ProductsScreen";
-import { Nav } from "../components/sharedComponents/nav/Nav";
+
 import AddressScreen from "../pages/AddressScreen/AddressScreen";
 
 const ProductsRouter = () => {
