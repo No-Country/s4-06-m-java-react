b9051449a9611e903e7f04ca768bbe34efdac74d
cristian mosquera <cmosqbone1@gmail.com>
2022-10-07 19:35:15 +0200

    adding admin control styles

--- src/pages/AdminControl.jsx
+++ src/pages/AdminControl.jsx
@@ -1,42 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from "react";
+
 import styled from "styled-components";
 
 const AdminControl = () => {
-  const add = {
-    shortDetails: "detalle2",
-    details: "detalle2",
-    title: "titulo",
-    brand: "Adidas",
-    view: "4545",
-    stock: true,
-    price: "200",
+  const [file, setfile] = useState(null);
+  const [Product, setProduct] = useState({
+    shortDetails: "",
+    details: "",
+    title: "",
+    brand: "",
+    view: "",
+    stock: "",
+    price: "",
     size: "S",
-    color: "RED",
-    categoryId: 5,
+    color: "",
+    categoryId: "",
+    isEditing: false,
+    editProductId: "",
+  });
+
+  const handlerUpload = (e) => {
+    setfile(e.target.files);
   };
+
+  const inputPrice = useRef();
+  const shortDetailsInput = useRef();
+  const detailsinput = useRef();
+  const titleInput = useRef();
+  const brandInput = useRef();
+  const selectSizeInput = useRef();
+
+  const handleSubmitAdd = (e) => {
+    e.preventDefault();
+    console.log("enviando todo al backend");
+    const formData = new FormData();
+    formData.append("postimages", file);
+
+    const title = titleInput.current.value;
+    const price = inputPrice.current.value;
+
+    const shortDetails = shortDetailsInput.current.value;
+
+    const details = detailsinput.current.value;
+    const brand = brandInput.current.value;
+    const selectsize = selectSizeInput.current.value;
+
+    const content = {
+      price,
+      shortDetails,
+      details,
+      title,
+      brand,
+      size: selectsize,
+    };
+    console.log(content);
+  };
+
   return (
     <Wrapper>
       <h1 className="title">ADMIN CONTROL</h1>
 
       <div className="control-buttons-admin">
-        <button className="control-buttons-admin__btn">EDITAR PRODUCTO</button>
-        <button className="control-buttons-admin__btn">EDITAR OTRA COSA</button>
-        <button className="control-buttons-admin__btn">EDITAR OTRA COSA</button>
-        <button className="control-buttons-admin__btn">EDITAR OTRA COSA</button>
+        <button className="control-buttons-admin__btn">AGREGAR PRODUCTO</button>
       </div>
 
-      <div className="wrapper-content">
+      <form className="wrapper-content">
         <label>IMAGEN</label>
-        <input type="file" className="file input" />
-        <label>precio</label>
-        <input type="texto" className="input" />
+        <input type="file" className="file input" onChange={handlerUpload} />
         <label>titulo</label>
-        <input type="texto" className="input" />
-        <label>Descripcion</label>
-        <input type="texto" className="input" />
-        <label>Descripcion</label>
-        <input type="texto" className="input" />
-      </div>
+        <input type="texto" className="input" ref={titleInput} />
+        <label>precio</label>
+        <input type="number" className="input" ref={inputPrice} />
+        <label>details</label>
+        <input type="texto" className="input" ref={detailsinput} />
+        <label>brand</label>
+        <input type="texto" className="input" ref={brandInput} />
+        <label>shortDetails</label>
+        <input type="texto" className="input" ref={shortDetailsInput} />
+        <label>size</label>
+        <select name="size" id="size" ref={selectSizeInput}>
+          <option value="S">S</option>
+          <option value="X">X</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+        </select>
+
+        <button
+          className="control-buttons-admin__btn--blue"
+          onClick={handleSubmitAdd}
+        >
+          AGREGAR PRODUCTO
+        </button>
+      </form>
     </Wrapper>
   );
 };
@@ -68,6 +124,17 @@ const Wrapper = styled.section`
     color: white;
     padding: 1rem;
     margin-top: 2rem;
+  }
+
+  .control-buttons-admin__btn--blue {
+    background-color: #216407;
+    color: white;
+    padding: 1rem;
+    margin-top: 2rem;
+    border: none;
+    margin: 0 auto;
+    display: block;
+    margin-top: 3rem;
   }
   .title {
     text-align: center;
--- src/utils/helpers/helpers.js
+++ src/utils/helpers/helpers.js
@@ -4,3 +4,30 @@ export const formatPrice = (number) => {
     currency: "USD",
   }).format(number / 100);
 };
+
+export const fileUpload = async (file = [], content) => {
+  if (!file) throw new Error("no tenemos ningun archivo a subir");
+
+  const Url = "https://sport-eco.herokuapp.com/product/add";
+
+  const formData = new FormData();
+  formData.append("postimages", file);
+  formData.append("product", content);
+
+  try {
+    const res = await fetch(Url, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!res.ok) throw new Error("no se puedo subir imagen ");
+
+    const data = await res.json();
+
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error.message);
+  }
+};
